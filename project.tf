9f5036796a209fb1f7896b9a207ffbe3f392af6f
Armando <aalorro@gmail.com>
2020-05-26 12:16:39 -0400

    initial commit

---
+++ project.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region  = "us-east-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow http and ssh inbound traffic"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.allow_http_ssh.name}"]
+  key_name = "alorro2020"
+  user_data = "file(user_data.sh)"
+
+  tags = {
+    Name = "terraform test"
+  }
+}
+
+output "public_ip" {
+    value = "${aws_instance.web.public_ip}"
+}
+
+output "private_ip" {
+    value = "${aws_instance.web.private_ip}"
+}
---
+++ user_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt=get install apache2 -y
+sudo apt-get install ruby -y
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+sudo apt-get install gdebi-core -y
+cd /tmp
+sudo wget https://aws-codedeploy-us-east-2.s3.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
